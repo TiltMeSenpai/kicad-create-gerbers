00847cf3c58792820b77b95d6c8db8e82f31a1d9
Josh Koike <jk@tilting.me>
2020-07-14 13:10:11 -0700

    Supply full path of plot_gerbers.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH="/usr/lib/kicad-nightly/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH"
 export PYTHONPATH="/usr/lib/kicad-nightly/lib/python3/dist-packages/:$PYTHONPATH"
 
-python3 `dirname $0`/plot_gerbers.py $1 $(git describe --tags)
+python3 /usr/bin/plot_gerbers.py $1 $(git describe --tags)
