e88e396a5d636dd2563443a4ee18b30004326e8b
Josh Koike <jk@tilting.me>
2020-07-14 13:13:34 -0700

    Manually pull git tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 export LD_LIBRARY_PATH="/usr/lib/kicad-nightly/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH"
 export PYTHONPATH="/usr/lib/kicad-nightly/lib/python3/dist-packages/:$PYTHONPATH"
 
-python3 /usr/bin/plot_gerbers.py $1 $(git describe --tags)
+git describe --tags || git fetch --tags
+python3 /usr/bin/plot_gerbers.py $1 $(git describe --tags || git rev-parse --short HEAD)
