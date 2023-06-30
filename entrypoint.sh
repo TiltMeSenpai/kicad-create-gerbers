48d000a0df0b2844c625a6115ffc29c2f79935dc
Joshua Koike <jk@tilting.me>
2023-06-29 20:26:29 -0700

    Ok, don't need to check status now

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ export PYTHONPATH="/usr/lib/kicad/lib/python3/dist-packages/:$PYTHONPATH"
 echo "Running in $(pwd)"
 # shhhhhh it's ok
 git config --global --add safe.directory /github/workspace
-git status
 
 git describe --tags || git fetch --tags
 python3 /usr/bin/plot_gerbers.py $1 $(git describe --tags || git rev-parse --short HEAD || echo "UNKNOWN")
